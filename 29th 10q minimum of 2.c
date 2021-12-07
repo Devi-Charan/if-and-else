a7be84275d805486528775b53f73259b73cd8d15
Devi-Charan <94948633+Devi-Charan@users.noreply.github.com>
2021-12-07 16:42:53 +0530

    Add files via upload

---
+++ 29th 10q minimum of 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("Enter a number for a= ");
+	scanf("%d",&a);
+	printf("Enter a number for b= ");
+	scanf("%d",&b);
+	if(a<b)
+	{
+		printf("a is minimum");
+	}
+	else
+	{
+		printf("b is minimum");
+	}
+}
