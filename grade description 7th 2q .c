b88d75455e304d373c60556b3a4017677b9c113e
Devi-Charan <94948633+Devi-Charan@users.noreply.github.com>
2021-12-07 16:26:52 +0530

    Add files via upload

---
+++ grade description 7th 2q .c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+main()
+{
+	char c;
+	printf("Enter the Grade: ");
+	scanf("%c",&c);
+	if(c=='E')
+	{
+		printf("Excellent");
+	}
+	if(c=='V')
+	{
+		printf("Very Good");
+	}
+	if(c=='G')
+	{
+		printf("Good");
+	}
+	if(c=='A')
+	{
+		printf("Average");
+	}
+	if(c=='F')
+	{
+		printf("Fail");
+	}
+}
+
+
+
+
+
